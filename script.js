7715b89afe87e5ae5672db255b91faea2051368c
Arash Kazerouni <arashkazerooni484@gmail.com>
2022-09-23 14:19:44 +0330

    todos will added to DOM onClick

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ let todoList = [];
 function containsAnyLetters(str) {
   return /[a-zA-Z]/.test(str);
 }
+
+// Add Todos to DOM Function
+const addToDOM = (todo) => {
+  const html = `
+    <div class="todo" id=${todo.id}>
+    <p class="todo-text" >${todo.name}</p>
+    <i class="fa-solid fa-check" ></i>
+    <i class="fa-solid fa-trash"></i>
+    </div>
+    `;
+  todos.insertAdjacentHTML("beforeend", html);
+};
 // Blue Print Of Todo Declared
 class Todo {
   constructor(name, id = 0, checkStatus = false) {
@@ -18,15 +30,17 @@ class Todo {
 submit.onclick = (e) => {
   e.preventDefault();
   if (!containsAnyLetters(input.value)) return;
+  todos.innerHTML = "";
   input.value.split("");
   //   if (input.value === "") return;
   const newTodo = new Todo(input.value, this.id, this.checkStatus);
   todoList.push(newTodo);
   //   Set current ID
-  for (let i = 0; i < todoList.length; i++) {
-    newTodo.id = i;
-  }
-    console.log(todoList);
+  for (let i = 0; i < todoList.length; i++) newTodo.id = i;
+  //   Add All Todos To DOM
+  todoList.forEach((todo) => {
+    addToDOM(todo);
+  });
   input.value = "";
 };
 // Handle EnterPress
