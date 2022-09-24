15b5f9de8fffd149fe029578e1de29430aad2805
Arash Kazerouni <arashkazerooni484@gmail.com>
2022-09-24 04:06:10 +0330

    specify todos id fixed

--- script.js
+++ script.js
@@ -9,20 +9,20 @@ function containsAnyLetters(str) {
 const saveToLocal = (array) => {
   localStorage.setItem("todo", JSON.stringify(array));
 };
-
 let todoList = JSON.parse(localStorage.getItem("todo") || "[]");
 console.log(todoList);
 // Add Todos to DOM Function
 const addToDOM = (array) => {
   todos.innerHTML = "";
-  array.forEach((todo) => {
+  array.forEach((todo, index) => {
     const html = `
-    <div class="todo" id=${todo.id}>
-    <p class="todo-text" >${todo.name}</p>
-    <i class="fa-solid fa-check" ></i>
+    <div class="todo" id=${index}>
+    <p class="todo-text ${todo.checkStatus ? "checked" : ""}" >${todo.name}</p>
+    <i class="fa-solid fa-check"></i>
     <i class="fa-solid fa-trash"></i>
     </div>
     `;
+    todo.id = index;
     todos.insertAdjacentHTML("beforeend", html);
   });
 };
@@ -36,15 +36,11 @@ class Todo {
 }
 // Handle Todo Submit
 addToDOM(todoList);
-
 submit.onclick = (e) => {
   e.preventDefault();
   if (!containsAnyLetters(input.value)) return;
   const newTodo = new Todo(input.value, this.id, this.checkStatus);
-  console.log(todoList);
   todoList.push(newTodo);
-  //   Set current ID
-  for (let i = 0; i < todoList.length; i++) newTodo.id = i;
   //   Add All Todos To DOM
   addToDOM(todoList);
   saveToLocal(todoList);
@@ -55,4 +51,3 @@ document.onkeypress = (e) => {
   if (e.key === "Enter") submit.click();
 };
 
-// Handle Check/Remove Items
